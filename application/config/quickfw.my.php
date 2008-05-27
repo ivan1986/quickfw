0966ca2d2edad1eb58959a0c4b56e42b30ee01d6
Ivan1986 <ivan1986@list.ru>
2008-05-27 13:19:01 +0000

    деквотер в конфиге

--- application/config/quickfw.my.php
+++ application/config/quickfw.my.php
@@ -38,4 +38,20 @@ $config['templater']= array(
 );
 /**/
 
+/*
+function strips(&$el) {
+  if (is_array($el)) 
+    foreach($el as $k=>$v) 
+      strips($el[$k]); 
+  else $el = stripslashes($el); 
+} 
+if (get_magic_quotes_gpc()) { 
+  strips($_GET);
+  strips($_POST);
+  strips($_COOKIE); 
+  strips($_REQUEST);
+  if (isset($_SERVER['PHP_AUTH_USER'])) strips($_SERVER['PHP_AUTH_USER']); 
+  if (isset($_SERVER['PHP_AUTH_PW']))   strips($_SERVER['PHP_AUTH_PW']);
+}/**/
+
 ?>
