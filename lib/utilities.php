fe011eea6db53c042e5905fd3c47e372e2021fcf
Ivan1986 <ivan1986@list.ru>
2008-10-01 11:44:46 +0000

    + Полезные функции

--- lib/utilities.php
+++ lib/utilities.php
@@ -6,7 +6,7 @@
  *@var string url адрес запроса
  *@var string userAgent кем представляться серверу
  *@return string|false Возвращает: ответ сервера (без заголовков)или false в случае неудачи
- */	
+ */
 function getURLContent($url, $userAgent = 'SeriousDron Utilities Pack, getURLContent function')
 {
 	//Закоментил чтобы передавать ЖЖ данные бота, а это может только CURL
@@ -19,21 +19,67 @@ function getURLContent($url, $userAgent = 'SeriousDron Utilities Pack, getURLCon
 			curl_close($curl);
 			return false;
 		}
-		
+
 		curl_setopt($curl, CURLOPT_HEADER, false);			//заголовки не нужны
 		/*curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);	//переходить по 'Location:'*/
-		
+
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);	//Вернуть результат
 		curl_setopt($curl, CURLOPT_USERAGENT, $userAgent);	//Передать наш юзер-агент
-		
+
 		$content = curl_exec($curl);
 		$curlErr = curl_errno($curl);
-		
+
 		curl_close($curl);
-		
+
 		if ($curlErr != CURLE_OK)
 			return false;
 
 		return $content;
 }
+
+/**
+ * 	Вывод сообщения с разбивкой длинных слов без повреждения тегов
+ */
+function msg2html($s) {
+
+	$s=make_urls(htmlspecialchars($s, ENT_QUOTES, 'utf-8'));
+
+	$marker = " <> ";
+
+	# Сохраняем все тэги чтобы уберечь их от разбивки
+	preg_match_all("/(<.*?>)/si",$s,$tags);
+
+	# Заменяем все тэги на маркеры
+	$s =  preg_replace("/(<.*?>)/si", $marker, $s);
+
+	$s = preg_replace('|\S{40}|u','\0 ',$s);
+
+	# Восстанавливаем тэги в места которых были отмечены маркерами
+	for ($i=0; $i<count($tags[0]);  $i++)
+		$s = preg_replace("/$marker/si", $tags[1][$i], $s, 1);
+
+	return $s;
+}
+
+/**
+ * 	Печать размера файла
+ */
+function printSize($size)
+{
+	if ($size>1024*10)
+		return (round($size/1024*100)/100).' Kb';
+	return $size.'b';
+}
+
+/**
+ * 	Усечение UTF8 строк
+ */
+function my_trim($srt,$size)
+{
+	if ((mb_strlen($srt)-3)>$size)
+		$srt=mb_substr($srt,0,$size-3).'...';
+	return $srt;
+}
+
+
 ?>
