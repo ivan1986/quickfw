308215105221f661cae19385bdfb58a07f153f2e
Ivan1986 <ivan1986@list.ru>
2008-11-27 13:52:21 +0000

    * Блин, про print_r забыл * Формат даты

--- lib/Log.php
+++ lib/Log.php
@@ -10,12 +10,11 @@ class Log
 		if (!array_key_exists($file,self::$_thisInst))
 			self::$_thisInst[$file] = new Log($file.'.log');
 		self::$_thisInst[$file]->l($str);
-		print_r(self::$_thisInst);
 	}
 
 	public function l($str)
 	{
-		file_put_contents($this->file, date("d.m.Y H:i:s").': '.$str."\n", FILE_APPEND|LOCK_EX);
+		file_put_contents($this->file, date('Y-m-d H:i:s').': '.$str."\n", FILE_APPEND | LOCK_EX);
 	}
 
 	public function __construct($filename)
