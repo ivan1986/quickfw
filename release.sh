617946b17159b93b0873606c63915da2443f81f2
Ivan1986 <ivan1986@list.ru>
2009-01-18 19:00:10 +0000

    * Скрипт упаковки релиза

---
+++ release.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#DESC Скрипт создания релиза, первый параметр - имя релиза (например QuickFWv0.7b => QuickFWv0.7b.tar.gz)
+
+test -z "$1" && echo "usage release.sh <releaseName>" && exit 1
+
+cd `dirname $0`
+olddir=`pwd`
+
+cd ..
+rm -rf $1
+cp -R $olddir/ ./$1
+cd $1
+
+find ./ -name '.svn' | xargs rm -rf $1
+find ./doc/* -type d | xargs rm -rf $1
+rm -rf addons
+rm -rf lib/datagrid
+rm -rf www/datagrid
+./prem.sh 777
+rm -f release.sh
+
+cd ..
+rm -f $1.tar.gz
+tar -czf $1.tar.gz $1
+rm -rf $1
