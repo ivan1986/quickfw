4ad2d737b70fb9ccd2d9b61c1ee4d01b76a2401e
Ivan1986 <ivan1986@list.ru>
2009-02-19 10:03:08 +0000

    * Причесан jabber бот

---
+++ lib/jabber/jabber.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'XMPPHP/XMPP.php';
+
+QFW::$libs['jabber'] = new XMPPHP_XMPP(
+	QFW::$config['jabber']['host'], QFW::$config['jabber']['port'],
+	QFW::$config['jabber']['user'], QFW::$config['jabber']['pass'],
+	QFW::$config['jabber']['resource'], QFW::$config['jabber']['server'],
+	true, XMPPHP_Log::LEVEL_INFO);
+
+?>
