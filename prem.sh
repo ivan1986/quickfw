b863f5b70730d126697ac03ca50312cb389e2f4a
Ivan1986 <ivan1986@list.ru>
2009-09-10 18:31:26 +0000

    * Создание папок в скрипте - удобно при развертывании из git репозитирия

--- prem.sh
+++ prem.sh
@@ -5,6 +5,10 @@ cd `dirname $0`
 
 test -z "$1" && echo "usage prem.sh <rights>" && exit 1
 
+# нужно если используется git - он не создает папки
+mkdir -p tmp/cache
+mkdir -p log
+
 chmod $1 -R tmp
 chmod $1 log
 chmod $1 tests/Smarty/cache
