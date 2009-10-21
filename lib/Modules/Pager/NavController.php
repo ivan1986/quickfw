1916c0a89800fb93c79d758f4536d232db93470c
Ivan1986 <ivan1986@list.ru>
2009-10-21 09:24:22 +0000

    * Модуль пагинатора и новый шаблон

--- lib/Modules/Pager/NavController.php
+++ lib/Modules/Pager/NavController.php
@@ -3,17 +3,18 @@
 class NavController
 {
 
-	public function pagerBlock($url='$',$all=0,$cur=1)
+	public function pagerBlock($url='$',$all=0,$cur=1,$size=5)
 	{
-		if ($all<2) return "";
+		if ($all<2) return '';
 		QFW::$view->assign('pager',array(
 			'all'=>$all,
 			'c'=>$cur,
 			'url'=>$url,
+			'size'=>$size,
 		));
 		return QFW::$view->fetch('pager.html');
 	}
-	
+
 }
 
 ?>
