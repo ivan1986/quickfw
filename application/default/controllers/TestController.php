50ae6b0527f2a39fda4df3adfa8dad0660eb6242
Ivan1986 <ivan1986@list.ru>
2010-03-09 08:45:05 +0000

    * Удалил из теста мусор

--- application/default/controllers/TestController.php
+++ application/default/controllers/TestController.php
@@ -25,60 +25,6 @@ class TestController
 			QFW::$router->UriPath.' '.QFW::$router->CurPath.' '.QFW::$router->ParentPath;
 	}
 
-	public function aCli()
-	{
-		QFW::$view->mainTemplate = '';
-		require_once LIBPATH.'/Curl.php';
-		$C = new Curl(TMPPATH.'/cookie');
-		$data = $C->get('http://u1.imperion.org/');
-		$pos = mb_strpos($data->body, 'Ivan1986');
-		if (!$pos)
-		{
-			$form = $data->forms(0);
-			$f = $form['fields'];
-			$f['player[name]'] = 'Ivan1986';
-			$f['player[pw]'] = '1qazxsw2';
-			$data = $C->post('http://u1.imperion.org'.$form['action'], $f);
-			$data = $C->get('http://u1.imperion.org/');
-		}
-
-		$planets = array(
-			'93919407',
-			'93919406',
-			'93919402',
-			'93999501',
-			'93999502',
-			'93999507',
-			'93999508',
-			'93839201',
-			'93839207',
-			'93839208',
-		);
-		$planet = array_rand($planets);
-		$planet = $planets[$planet];
-
-		$data = $C->get('http://u1.imperion.org/fleetBase/mission/1/planetId/'.$planet);
-		$form = $data->forms(0);
-		$f = $form['fields'];
-
-		$m = array();
-		preg_match('#shipCount_5">\(([0-9]+)\)</a>#', $data, $m);
-		if (!empty($m[1]))
-		{
-			$d = array(
-				'f' => $f['f'],
-				'mission' => '302',
-				'planet' => $planet,
-				'ship[5]' => $m[1],
-				'tan' => $f['tan'],
-				'tank_text' => '',
-				'targetType' => 'p',
-			);
-			$data = $C->post('http://u1.imperion.org'.$form['action'], $d);
-		}
-	}
-	
-
 }
 
 ?>
