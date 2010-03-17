31442a7f65d2a058223a7ac1f10ec11dc249a6a4
Ivan1986 <ivan1986@list.ru>
2010-03-17 07:58:31 +0000

    * Интерфейс к Memcached

---
+++ QFW/Cacher/Memcached.php
@@ -0,0 +1,63 @@
+<?php
+
+class Cacher_Memcached implements Zend_Cache_Backend_Interface
+{
+	protected $mc;
+
+	public function __construct()
+	{
+		$this->mc = new Memcached;
+	}
+
+	public function setDirectives($directives)
+	{
+		if(!isset($directives['servers']))
+		{
+			$this->mc->addServer(
+				isset($directives['host']) ? $directives['host'] : 'localhost',
+				isset($directives['port']) ? $directives['port'] : '11211');
+			return;
+		}
+
+		foreach($directives['servers'] as $server)
+			$this->mc->addServer($server['host'], $server['port']);
+	}
+
+	public function addServer($host='localhost', $port=11211)
+	{
+		return $this->mc->addServer($host, $port);
+	}
+
+	public function save($data, $id, $tags = array(), $specificLifetime = 3600)
+	{
+		$this->mc->set($id, $data, $specificLifetime);
+	}
+
+	public function load($id, $doNotTest = false)
+	{
+		return $this->mc->get($id);
+	}
+
+	public function test($id)
+	{
+		return ($this->load($id)!==false);
+	}
+
+	public function getStats()
+	{
+		return $this->mc->getExtendedStats();
+	}
+
+	public function remove($id)
+	{
+		$this->mc->delete($id, 0);
+	}
+
+	public function clean($mode = CACHE_CLR_ALL, $tags = array())
+	{
+		if ($mode == CACHE_CLR_ALL)
+			$this->mc->flush();
+	}
+}
+
+?>
