53922c5a78859feef3b94595404d00af323c506c
Ivan1986 <ivan1986@list.ru>
2010-03-31 12:47:48 +0400

    Настройка скрипта расширений - папки создавать не нужно

--- perm.sh
+++ perm.sh
@@ -5,11 +5,5 @@ cd `dirname $0`
 
 test -z "$1" && echo "usage prem.sh <rights>" && exit 1
 
-# нужно если используется git - он не создает папки
-mkdir -p tmp/cache
-mkdir -p log
-
 chmod $1 -R tmp
 chmod $1 log
-chmod $1 tests/Smarty/cache
-chmod $1 tests/Smarty/templates_c
