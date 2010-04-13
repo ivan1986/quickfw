9067b3831d39f9e27514172e82ebef095a5679bd
Ivan1986 <ivan1986@list.ru>
2010-04-13 15:39:08 +0400

    Обновлен скрипт сборки архива

--- release.sh
+++ release.sh
@@ -8,17 +8,17 @@ olddir=`pwd`
 
 cd ..
 rm -rf $1
-svn export $olddir/ ./$1
-cd $1
+mkdir $1
+cd $olddir
+git archive --format=tar master | ( cd "../$1" ; tar xf - )
+cd ../$1
 
-find ./ -name '.svn' | xargs rm -rf $1
 find ./doc/* -type d | grep -v asciidoc | xargs rm -rf $1
 find ./tmp/* -type f | xargs rm -rf $1
 rm -rf addons
-rm -rf lib/datagrid
-rm -rf www/datagrid
 ./perm.sh 777
 rm -f release.sh
+rm -f .gitignore
 
 cd ..
 rm -f $1.tar.gz
