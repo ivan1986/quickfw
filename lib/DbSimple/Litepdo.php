44bf1fc5d50edb27caf449b1c04bf488884cf351
Ivan Borzenkov <ivan1986@list.ru>
2010-08-07 19:47:00 +0400

    Удалены *pdo_Blob за ненадобностью

--- lib/DbSimple/Litepdo.php
+++ lib/DbSimple/Litepdo.php
@@ -151,39 +151,4 @@ class DbSimple_Litepdo extends DbSimple_Generic_Database
 
 }
 
-class DbSimple_Mypdo_Blob implements DbSimple_Generic_Blob
-{
-	// MySQL does not support separate BLOB fetching.
-	private $blobdata = null;
-	private $curSeek = 0;
-
-	public function __construct(&$database, $blobdata=null)
-	{
-		$this->blobdata = $blobdata;
-		$this->curSeek = 0;
-	}
-
-	public function read($len)
-	{
-		$p = $this->curSeek;
-		$this->curSeek = min($this->curSeek + $len, strlen($this->blobdata));
-		return substr($this->blobdata, $this->curSeek, $len);
-	}
-
-	public function write($data)
-	{
-		$this->blobdata .= $data;
-	}
-
-	public function close()
-	{
-		return $this->blobdata;
-	}
-
-	public function length()
-	{
-		return strlen($this->blobdata);
-	}
-}
-
 ?>
