0b194a53c25c71167f7227ec241b3e431b8d7b45
Ivan Borzenkov <ivan1986@list.ru>
2010-11-22 12:41:04 +0300

    Объединил в одну

--- www/index.php
+++ www/index.php
@@ -3,12 +3,12 @@
 	define ('ROOTPATH', dirname(dirname(__FILE__)));
 	define ('APPPATH', ROOTPATH . '/application');
 	define ('VARPATH', ROOTPATH . '/var');
+	define ('QFWPATH', ROOTPATH . '/QFW');
+	define ('LIBPATH', ROOTPATH . '/lib');
+
 //Настройки по умолчанию, если нужно - измените
 //	define ('TMPPATH', VARPATH  . '/tmp');
 //	define ('LOGPATH', VARPATH  . '/log');
-	define ('QFWPATH', ROOTPATH . '/QFW');
-	define ('LIBPATH', ROOTPATH . '/lib');
-//Настройки по умолчанию, если нужно - измените
 //	define ('MODPATH', APPPATH  . '/_common/models');
 	
 	require (QFWPATH.'/Init.php');
