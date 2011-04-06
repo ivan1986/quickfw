627ae4c38d52c27683a3658f5ad41f79dc121a5a
Ivan Borzenkov <ivan1986@list.ru>
2011-04-06 05:33:23 +0400

    В доке описана подгрузка по требованию

--- QFW/QuickFW/Config.php
+++ QFW/QuickFW/Config.php
@@ -46,29 +46,35 @@ class QuickFW_Config implements ArrayAccess
 
 	public function offsetGet($offset) {
 		if ($this->dir && !isset($this->data[$offset]))
-		{
-			//пробуем загрузить файл
-			$file = $this->dir.'/'.$offset.'.php';
-			$data = array();
-			if (is_file($file))
-				$data = include($file);
-			if ($data == 1 && isset($config))
-				$data = $config;
-			$hf = $this->dir.'/'.$offset.'.'.$_SERVER['HTTP_HOST'].'.php';
-			if (is_file($hf))
-			{
-				$data = include($hf);
-				if ($data == 1 && isset($config))
-					$data = $config;
-			}
-			$this->data[$offset] = is_object($data) ? $data : (
-				is_array($data) ? new self($data, $this->dir.'/'.$offset) :
-				$data);
-		}
+			$this->data[$offset] = $this->loadFromFile($offset);
 		return $this->data[$offset];
 	}
 	public function offsetExists($offset) { return isset($this->data[$offset]); }
 	public function offsetUnset($offset) { unset($this->data); }
+
+	/**
+	 * Пробуем загрузить файл из текущей директории
+	 *
+	 * @param string $name имя файла
+	 * @return array|mixed|QuickFW_Config вложенный массив
+	 */
+	private function loadFromFile($name)
+	{
+		$file = $this->dir.'/'.$name.'.php';
+		$data = array();
+		if (is_file($file))
+			$data = include($file);
+		if ($data == 1 && isset($config))
+			$data = $config;
+		$hf = $this->dir.'/'.$name.'.'.$_SERVER['HTTP_HOST'].'.php';
+		if (is_file($hf))
+		{
+			$data = include($hf);
+			if ($data == 1 && isset($config))
+				$data = $config;
+		}
+		return is_array($data) ? new self($data, $this->dir.'/'.$name) : $data;
+	}
 
 	//as prop
 	public function __get($offset) { return $this->offsetGet($offset); }
