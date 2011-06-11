a64d640d1933b3d598afff24fac0c2a8e8f85016
Ivan Borzenkov <ivan1986@list.ru>
2011-06-12 01:07:56 +0400

    Используется array_replace_recursive

--- QFW/QuickFW/Config.php
+++ QFW/QuickFW/Config.php
@@ -120,7 +120,7 @@ class QuickFW_Config implements ArrayAccess
 				$new = $config;
 			if (!empty($new))
 				$data = (is_array($data) && is_array($new)) ?
-					array_merge_replace_recursive($data, $new) : $new;
+					array_replace_recursive($data, $new) : $new;
 		}
 		return $data;
 	}
@@ -131,37 +131,4 @@ class QuickFW_Config implements ArrayAccess
 	public function __isset($offset) { return $this->offsetExists($offset); }
 	public function __unset($offset) { return $this->offsetUnset($offset); }
 
-}
-
-/**
- * Merges any number of arrays of any dimensions, the later overwriting
- * previous keys, unless the key is numeric, in whitch case, duplicated
- * values will not be added.
- *
- * The arrays to be merged are passed as arguments to the function.
- *
- * @access public
- * @return array Resulting array, once all have been merged
- */
-function array_merge_replace_recursive()
-{
-	// Holds all the arrays passed
-	$params = func_get_args();
-	// First array is used as the base, everything else overwrites on it
-	$return = array_shift($params);
-	// Merge all arrays on the first array
-	foreach ($params as $array)
-		foreach ($array as $key => $value)
-			// Numeric keyed values are added (unless already there)
-			if (is_numeric($key) && (!in_array($value, $return)))
-				if (is_array($value))
-					$return[] = array_merge_replace_recursive($return[$key], $value);
-				else
-					$return[] = $value;
-			else // String keyed values are replaced
-				if (isset($return[$key]) && is_array($value) && is_array($return[$key]))
-					$return[$key] = array_merge_replace_recursive($return[$key], $value);
-				else
-					$return[$key] = $value;
-	return $return;
 }
